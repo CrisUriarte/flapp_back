a6865b49955fe58f4aa62201613d1d80709dba4e
CrisUriarte <cristobal.uriarte@hotmail.com>
2025-02-05 01:02:58 -0300

    cors-front

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://flapp-front.onrender.com",  // Permitir frontend
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+  })
+);
 app.use(express.json());
 
 
